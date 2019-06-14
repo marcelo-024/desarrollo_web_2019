40902312db7f10a0599902307d575d43c461c516
marcelo <marcelomaderuelosacido@gmail.com>
2019-06-14 21:14:00 +0200

    BASIC PUG

---
+++ 10_intro_js/12_bucle.js
@@ -0,0 +1,34 @@
+/* let limite = 10
+
+let suma = 0
+for (let i = 0; i < limite; i++) {
+  let cuad = i*i
+    console.log(i, cuad)
+    suma = suma + cuad
+}
+
+console.log (suma)
+ */
+
+
+
+const aDatos = [
+
+        'Perro',
+        'Gato',
+        'Pepe',
+        'Ratón'
+]
+
+
+for (let i=0;i< aDatos.length; i++) {
+
+let item = aDatos [i];
+
+if (i > 0) {
+    item = item + 's'
+}
+
+console.log (`Tengo ${i+1} ${item}`)
+
+}
