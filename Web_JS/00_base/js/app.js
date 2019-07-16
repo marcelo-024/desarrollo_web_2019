88f98044983111aa6f89c8f5caf5ad190b117b06
marcelo-024 <marcelomaderuelosacido@gmail.com>
2019-07-16 16:46:09 +0200

    Base JSWEB

---
+++ Web_JS/00_base/js/app.js
@@ -0,0 +1,5 @@
+console.log('Hola, estoy aquí')
+/* console.dir('Buscando nodos')
+console.error('Esto es un error')
+console.info('Dame datos')
+console.table(['Hey esto ser tabla','Esto tambien','Esto tambien']) */
